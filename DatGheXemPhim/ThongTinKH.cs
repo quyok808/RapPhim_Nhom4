42d41b0afc0361db09d61d2d2e52b8d0a4c2b33f
CongQuy252 <nva2572003@gmail.com>
2023-10-04 16:39:51 +0700

    Fix them khach hang - Bang

--- DatGheXemPhim/ThongTinKH.cs
+++ DatGheXemPhim/ThongTinKH.cs
@@ -16,37 +16,13 @@ namespace DatGheXemPhim
     {
         public ThongTinKH()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
         static RapPhimModel rp = new RapPhimModel();
-        public bool checkNull()
-        {
-            if (txtName.Text == "" || txtSDT.Text == "")
-                return false;
-            return true;
-        }
         private void btnXacNhan_Click(object sender, EventArgs e)
         {
-            try
-            {
-                if (checkNull() == false)
-                {
-                    MessageBox.Show("Chưa nhập đủ thông tin", "Thông Báo");
-                }
-                else
-                {
-                    KhachHang kh = new KhachHang();
-                    {
-                        kh.TenKH = txtName.Text;
-                        kh.SDT = txtSDT.Text;
-                    };
-                    rp.KhachHangs.Add(kh);
-                    rp.SaveChanges();
-                    MessageBox.Show("Thêm thành công", "Thông báo", MessageBoxButtons.OK);
-                    this.Close();
-                }
-            }
-            catch { }
+            DialogResult = DialogResult.OK;
+            this.Close();
         }
 
         private void btnHuy_Click(object sender, EventArgs e)
