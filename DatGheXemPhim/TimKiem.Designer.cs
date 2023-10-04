38b62ffd94861c17cfcd084d3c3a41553e9e9656
CongQuy252 <nva2572003@gmail.com>
2023-10-04 21:03:40 +0700

    them tim

---
+++ DatGheXemPhim/TimKiem.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace DatGheXemPhim
+{
+    partial class TimKiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimKiem));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKeynum = new System.Windows.Forms.RichTextBox();
+            this.txtKeyname = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Find_Button = new System.Windows.Forms.Button();
+            this.Back_Button = new System.Windows.Forms.Button();
+            this.ColumnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtKeynum);
+            this.groupBox1.Controls.Add(this.txtKeyname);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(373, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 138);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin tìm kiếm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(39, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SĐT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(18, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Họ và tên";
+            // 
+            // txtKeynum
+            // 
+            this.txtKeynum.Location = new System.Drawing.Point(114, 83);
+            this.txtKeynum.Name = "txtKeynum";
+            this.txtKeynum.Size = new System.Drawing.Size(210, 27);
+            this.txtKeynum.TabIndex = 0;
+            this.txtKeynum.Text = "";
+            // 
+            // txtKeyname
+            // 
+            this.txtKeyname.Location = new System.Drawing.Point(114, 40);
+            this.txtKeyname.Name = "txtKeyname";
+            this.txtKeyname.Size = new System.Drawing.Size(210, 27);
+            this.txtKeyname.TabIndex = 0;
+            this.txtKeyname.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnName,
+            this.ColumnNum,
+            this.ColumnID});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 245);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1092, 226);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Find_Button
+            // 
+            this.Find_Button.BackColor = System.Drawing.Color.Yellow;
+            this.Find_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Find_Button.Location = new System.Drawing.Point(499, 184);
+            this.Find_Button.Name = "Find_Button";
+            this.Find_Button.Size = new System.Drawing.Size(108, 37);
+            this.Find_Button.TabIndex = 2;
+            this.Find_Button.Text = "Tìm kiếm";
+            this.Find_Button.UseVisualStyleBackColor = false;
+            this.Find_Button.Click += new System.EventHandler(this.Find_Click);
+            // 
+            // Back_Button
+            // 
+            this.Back_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Back_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Back_Button.Location = new System.Drawing.Point(950, 184);
+            this.Back_Button.Name = "Back_Button";
+            this.Back_Button.Size = new System.Drawing.Size(108, 37);
+            this.Back_Button.TabIndex = 2;
+            this.Back_Button.Text = "Trở về";
+            this.Back_Button.UseVisualStyleBackColor = false;
+            this.Back_Button.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // ColumnName
+            // 
+            this.ColumnName.HeaderText = "Tên Khách Hàng";
+            this.ColumnName.MinimumWidth = 6;
+            this.ColumnName.Name = "ColumnName";
+            this.ColumnName.Width = 370;
+            // 
+            // ColumnNum
+            // 
+            this.ColumnNum.HeaderText = "SĐT";
+            this.ColumnNum.MinimumWidth = 6;
+            this.ColumnNum.Name = "ColumnNum";
+            this.ColumnNum.Width = 250;
+            // 
+            // ColumnID
+            // 
+            this.ColumnID.HeaderText = "Mã ghế";
+            this.ColumnID.MinimumWidth = 6;
+            this.ColumnID.Name = "ColumnID";
+            this.ColumnID.Width = 170;
+            // 
+            // TimKiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 469);
+            this.Controls.Add(this.Back_Button);
+            this.Controls.Add(this.Find_Button);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TimKiem";
+            this.Text = "Tim Kiem KH";
+            this.Load += new System.EventHandler(this.TimKiem_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox txtKeyname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtKeynum;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Find_Button;
+        private System.Windows.Forms.Button Back_Button;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnID;
+    }
+}
